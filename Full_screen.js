9dc7f09a53eddc1d673787e5e04c5f647d1b1dac
Zorrofoxline <nicofirulais584@gmail.com>
2025-04-24 19:05:40 -0500

    Update Full_screen.js

--- Full_screen.js
+++ Full_screen.js
@@ -1,2 +1,11 @@
-document.documentElement.requestFullscreen();
+function launchFullScreen(element) {
+  if(element.requestFullScreen) {
+    element.requestFullScreen();
+  } else if(element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if(element.webkitRequestFullScreen) {
+    element.webkitRequestFullScreen();
+  }
+}
+launchFullScreen(document.documentElement);
 alert("full screen press back or swipe left or right to exit");
